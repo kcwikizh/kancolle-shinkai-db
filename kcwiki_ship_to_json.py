8aeaa4ee36743dd43f1424cffcf645c5fac4daff
McZoden <summersun.sdc@gmail.com>
2017-10-24 16:57:13 +0800

    Use import utils to reuse exception and function

--- kcwiki_ship_to_json.py
+++ kcwiki_ship_to_json.py
@@ -1,7 +1,7 @@
 '''Convert KcWiki ship data to json
 '''
 import re
-import json
+from utils import MyException, nedb_parser
 
 KCWIKI_SHINKAI_SHIPS = 'original_data/kcwiki_shinkai_ships.txt'
 NEDB_EQUIPS = 'db/equips.nedb'
@@ -43,40 +43,6 @@ SHIP_RANGE_MAPPING_TABLE = {
     '长': 3,
     '超长': 4
 }
-
-class MyException(Exception):
-    '''My Exception
-
-    I don't want to check the return value of every function when error occurs.
-    Raise an exception, just easier to programming.
-    '''
-    def __init__(self, msg):
-        super().__init__(msg)
-        self.message = msg
-
-    def __str__(self):
-        return self.message
-
-
-def nedb_parser(nedb):
-    '''nedb_parser
-    '''
-    result = {}
-    line_num = 1
-
-    print('Get raw data from {}'.format(nedb))
-    with open(nedb, 'r', encoding='utf-8') as nedb_f:
-        for line in nedb_f:
-            python_object = json.loads(line)
-            if not isinstance(python_object, dict):
-                raise MyException('Not a python dict, line number {}'.format(line_num))
-
-            line_num += 1
-            item_id = python_object['id']
-            result[item_id] = python_object
-
-    print('Loaded {} datas from {}'.format(len(result), nedb))
-    return result
 
 
 def kcwiki_ship_get_id(ship, line):
