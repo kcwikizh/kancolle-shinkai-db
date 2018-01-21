9b92a4ffe584761217f86a0993d482a2b2ec98b6
McZoden <summersun.sdc@gmail.com>
2018-01-21 17:16:06 +0800

    [function] support bool

--- utils.py
+++ utils.py
@@ -45,7 +45,7 @@ def nedb_parser(nedb):
 def python_data_to_lua_table(data, level=0, indent=LUA_INDENT):
     """Generate Lua string via python structure
 
-    only dict, list, string, number are allowed
+    only dict, list, string, number, bool are allowed
 
     :param data: data to parse
     :param level: indent level
@@ -71,6 +71,10 @@ def python_data_to_lua_table(data, level=0, indent=LUA_INDENT):
                     lines.append(level*indent + '{\n'
                                  + line + '\n' + level*indent + '}')
                 all_elements_is_pure_data = False
+            elif isinstance(i, bool):
+                # this must before of int case
+                lines.append(level*indent + '{}'.format(
+                    'true' if i else 'false'))
             elif isinstance(i, int):
                 lines.append(level*indent + str(i))
             elif isinstance(i, str):
@@ -99,6 +103,10 @@ def python_data_to_lua_table(data, level=0, indent=LUA_INDENT):
                 else:
                     lines.append(level*indent + '["{}"] = {{\n'.format(i)
                                  + line + '\n' + level*indent + '}')
+            elif isinstance(data[i], bool):
+                # this must before of int case
+                lines.append(level*indent + '["{}"] = {}'.format(i,
+                    'true' if data[i] else 'false'))
             elif isinstance(data[i], int):
                 lines.append(level*indent + '["{}"] = {}'.format(i, data[i]))
             elif isinstance(data[i], str):
